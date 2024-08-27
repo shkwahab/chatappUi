844d91e07bd4c919e994ddcf0fc1b31dbdc9ed44
shkwahab <wahab209266@gmail.com>
2024-08-27 15:57:00 +0500

    test webpush data

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,25 +1,28 @@
 self.addEventListener('push', (event) => {
     console.log('Push event received:', event);
-
-    if (event.data) {
-        console.log('Push data:', event.data.text()); // or .json() if JSON
-        event.data.json().then(data => {
-            const title = data?.title || 'New Notification';
-            const options = {
-                body: data?.body || 'You have a new message!',
-                icon: '/favicon.svg',
-                data: data?.url || '/',
-            };
+    console.log("test as data json")
+console.log(event?.data?.json)
+console.log("test as data")
+console.log(event?.data)
+    // if (event.data) {
+    //     console.log('Push data:', event.data.text()); // or .json() if JSON
+    //     event.data.json().then(data => {
+    //         const title = data?.title || 'New Notification';
+    //         const options = {
+    //             body: data?.body || 'You have a new message!',
+    //             icon: '/favicon.svg',
+    //             data: data?.url || '/',
+    //         };
             
-            event.waitUntil(
-                self.registration.showNotification(title, options).catch(error => {
-                    console.error('Error showing notification:', error);
-                })
-            );
-        }).catch(error => {
-            console.error('Error parsing push data:', error);
-        });
-    } else {
-        console.error('No data found in push event');
-    }
+    //         event.waitUntil(
+    //             self.registration.showNotification(title, options).catch(error => {
+    //                 console.error('Error showing notification:', error);
+    //             })
+    //         );
+    //     }).catch(error => {
+    //         console.error('Error parsing push data:', error);
+    //     });
+    // } else {
+    //     console.error('No data found in push event');
+    // }
 });
